619592f65f748595fc6b9a520b31d5fee730f025
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-02-20 19:53:57 -0800

    Add dropdown menu functionality with correct closing behavior

--- client/src/components/StarRatingFilter.jsx
+++ client/src/components/StarRatingFilter.jsx
@@ -1,19 +1,53 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
 import style from './StarRatingFilter.css';
 
 function StarRatingFilter() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  let callState = (event) => {
+    if (!document.getElementById('ratingContainerStars').contains(event.target)) {
+      setDropdownOpen(!dropdownOpen);
+      document.removeEventListener('mousedown', callState);
+    } else if (document.getElementById('starFilterContainer').contains(event.target)) {
+      document.removeEventListener('mousedown', callState);
+    }
+  };
+  callState = callState.bind(this);
+
+
+  (function eventer() {
+    if (dropdownOpen) {
+      document.addEventListener('mousedown', callState);
+    }
+  }());
+
   return (
-    // this div is going to contain a list that
-    //  is visible when the mouse is hovering
-    //  the font needs to be red on hover too
-    <div className={style.FilterBoxDiv}>
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href="#">
-        <span className={style.FilterTitle}>
-          Star Rating
-          <img src="https://s3.us-east-2.amazonaws.com/ouiqln-review-img/assets/downarrowbold.png" alt="down arrow" />
-        </span>
-      </a>
+    <div id="ratingContainerStars" className={style.mycontainer}>
+      <div
+        id="starFilterContainer"
+        className={style.FilterBoxDiv}
+        onClick={() => { setDropdownOpen(!dropdownOpen); }}
+      >
+        <a href="#">
+          <span className={style.FilterTitle}>
+            Star Rating
+            <img src="https://s3.us-east-2.amazonaws.com/ouiqln-review-img/assets/downarrowbold.png" alt="down arrow" />
+          </span>
+        </a>
+      </div>
+      <div
+        id="dropdownStar"
+        className={style.blackBox}
+        style={dropdownOpen ? { display: 'block' } : {}}
+      >
+      Black Box
+        <ul>
+          <li><button type="button">hello</button></li>
+        </ul>
+      </div>
     </div>
   );
 }
