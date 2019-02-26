9d236d6a2a11d9ae6670f54b878a044e58949d38
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-02-25 17:43:09 -0800

    Add div structure to review attributes div

--- client/src/components/ReviewEntryComponents/RatingScales.jsx
+++ client/src/components/ReviewEntryComponents/RatingScales.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import style from './RatingScales.css';
 
 function RatingScales() {
-
   return (
-    <div>RatingScales</div>
+    <div className={style.RatingScale}>
+      <div className={style.RatingStars}>
+        {/* put the rating star function here */}
+        <div className={style.RatingTitle}>Rating</div>
+      </div>
+      <div className={style.RatingScaleContainer}>
+        {/* put the scales here, import */}
+      </div>
+    </div>
   );
 }
 
