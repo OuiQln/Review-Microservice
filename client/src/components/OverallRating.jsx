ac73a3a0982c856dc90a2ae5708ec43a4ce1b0a8
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-02-25 19:32:52 -0800

    Refactor OverallRating component for modularity, Build out RatingScales for the review entry

--- client/src/components/OverallRating.jsx
+++ client/src/components/OverallRating.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
-import style from './OverallRating.css';
 import RatingSlider from './RatingSlider';
+import RatingStars from './RatingStars';
+import style from './OverallRating.css';
 
 //  this component needs the aggregate data props for stars and avg scales
 
 function OverallRating() {
   // this funciton indicates we need total stars count and num of reviews props
-
-  function numOfReviews() {
-    return Math.floor(Math.random() * 100);
-  }
-
-  function totalStars(num) {
-    return ((Math.random() * 5) * num);
-  }
-
-  function rating(countStars, countReviews) {
-    const avgRating = (countStars / countReviews).toFixed(1);
-    const rtgArr = [...`${avgRating}`];
-    rtgArr[1] = '_';
-    return rtgArr.join('');
-  }
-
-  const reviewCount = numOfReviews();
-  const starCount = totalStars(reviewCount);
-
-  const avgStars = rating(starCount, reviewCount);
-  const starImg = `https://s3.us-east-2.amazonaws.com/ouiqln-review-img/review-stars/rating${avgStars}.png`;
-
   return (
     <>
       <div className={style.OverallRatingContainer}>
@@ -35,9 +14,7 @@ function OverallRating() {
           <div className={style.OverallRatingEntry}>
             <div className={style.RatingOverall}>
               <div className={style.Label}>Overall Rating</div>
-              <div className={style.NormalImage}>
-                <img src={starImg} alt={`Rating of ${avgStars}`} />
-              </div>
+              <RatingStars />
             </div>
           </div>
         </div>
