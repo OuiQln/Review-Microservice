2afc32c0699265cd5ee05c00ee3367fe0e252ec1
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-02-26 09:40:25 -0800

    Add basic pagination interaction

--- client/src/components/ReviewSection.jsx
+++ client/src/components/ReviewSection.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState } from 'react';
 import ReviewEntry from './ReviewEntry';
 import ReviewData from './reviewData';
+import style from './ReviewSection.css';
 
 function ReviewSection() {
 // this component needs to get the up to 4 currently visible reviews
-  function makeReviewTest() {
+  const [moreReviews, setMoreReviews] = useState(false);
+  
+  function makeReviewTest(start = 0, end = 4) {
     const reviewArr = [];
-    for (let i = 0; i < 4; i += 1) {
+    for (let i = start; i < end; i += 1) {
       let review = ReviewData[i];
       reviewArr.push(<ReviewEntry key={i} reviewData={review} />);
     }
     return reviewArr;
   }
-
+  
   return (
     <div>
-      {makeReviewTest()}
-      {/* <div>Paginator at the bottom</div> */}
+      {moreReviews ? makeReviewTest(4, 8) : makeReviewTest()}
+      <div onClick={() => setMoreReviews(!moreReviews)} className={style.Paginator}>
+        <img src="https://s3.us-east-2.amazonaws.com/ouiqln-review-img/assets/paginator.png" />
+      </div>
     </div>
   );
 }
